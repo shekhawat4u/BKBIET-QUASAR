3495de08efdf006001f8fc64357891b5037d965d
shekhawat4u <harsh281206@gmail.com>
2025-09-10 22:09:41 +0530

    TFyugs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -75,7 +75,7 @@ const Hero = () => {
       <section id="hero" className="noisy">
         <h1 className="title">QUASAR</h1>
 
-        <img
+        {/* <img
           src="/images/hero-left-leaf.png"
           alt="left-leaf"
           className="left-leaf"
@@ -84,7 +84,7 @@ const Hero = () => {
           src="/images/hero-right-leaf.png"
           alt="right-leaf"
           className="right-leaf"
-        />
+        /> */}
 
         <div className="body">
           {/* <img src="/images/arrow.png" alt="arrow" className="arrow" /> */}
